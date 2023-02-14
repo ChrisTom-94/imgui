c2fdfb4271493b09b3efc5b56e2777591bac9c9f
ChrisTom-94 <christian.tomasino.dev@gmail.com>
2023-02-14 23:08:55 +0100

    add premake5 to docking branch

---
+++ premake5.lua
@@ -0,0 +1,58 @@
+project "imgui"
+	kind "StaticLib"
+	language "C++"
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+		"imgui_tables.cpp",
+		"backends/imgui_impl_glfw.h",
+		"backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_opengl3.h",
+		"backends/imgui_impl_opengl3.cpp",
+		"backends/imgui_impl_vulkan.h",
+		"backends/imgui_impl_vulkan.cpp"
+	}
+
+	includedirs
+	{
+		"./",
+		"%{Includes['glfw']}",
+		"%{Includes['vulkan']}",
+	}
+
+	filter "action:vs*"
+		targetdir ("%{wks.location}/build/bin/VisualStudio/" .. outputdir .. "/Dependencies/%{prj.name}")
+		objdir ("%{wks.location}/build/bin-int/VisualStudio/" .. outputdir .. "/Dependencies/%{prj.name}")
+	
+	filter "action:gmake*"
+		targetdir ("%{wks.location}/build/bin/Make/" .. outputdir .. "/Dependencies/%{prj.name}")
+		objdir ("%{wks.location}/build/bin-int/Make/" .. outputdir .. "/Dependencies/%{prj.name}")
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
